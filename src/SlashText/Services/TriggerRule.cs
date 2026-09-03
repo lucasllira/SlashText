c436e8c8534824f8a888c72a724c60084838280f
lucasllira <113149653+lucasllira@users.noreply.github.com>
2026-09-03 20:59:36 -0300

    feat: manter somente barra como prefixo de atalhos (#38)

--- src/SlashText/Services/TriggerRule.cs
+++ src/SlashText/Services/TriggerRule.cs
@@ -7,7 +7,7 @@ public static class TriggerRule
     public const int MinimumLength = 2;
     public const int MaximumLength = 64;
 
-    public static bool IsSupportedPrefix(char value) => value is '/' or ':';
+    public static bool IsSupportedPrefix(char value) => value == '/';
 
     public static bool IsSupportedCharacter(char value) =>
         char.IsLetterOrDigit(value) || value is '-' or '_';
@@ -22,7 +22,7 @@ public static class TriggerRule
 
         if (value.Length < MinimumLength)
         {
-            error = "O atalho precisa ter / ou : e pelo menos um caractere.";
+            error = "O atalho precisa começar com / e ter pelo menos um caractere.";
             return false;
         }
 
@@ -35,7 +35,7 @@ public static class TriggerRule
         if (!IsSupportedPrefix(value[0]) ||
             value.Skip(1).Any(character => !IsSupportedCharacter(character)))
         {
-            error = "O atalho deve começar com / ou : e usar somente letras, números, hífen ou sublinhado.";
+            error = "O atalho deve começar com / e usar somente letras, números, hífen ou sublinhado.";
             return false;
         }
 
